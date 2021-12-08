695736cb982bdc397496907a96d1de2567982c7d
Ilya Barouski <birily9@gmail.com>
2021-12-08 22:17:25 +0300

    Java start

---
+++ project1/src/com/learnidea/Main.java
@@ -0,0 +1,10 @@
+package com.learnidea;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+        String helloWorld = "Hello world";
+        System.out.println(helloWorld);
+    }
+}
