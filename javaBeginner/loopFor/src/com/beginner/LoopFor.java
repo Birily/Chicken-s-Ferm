de1011cf519366f511b6db25050566693fc6d87a
Ilya Barouski <birily9@gmail.com>
2021-12-15 22:45:02 +0300

    loopFor

---
+++ javaBeginner/loopFor/src/com/beginner/LoopFor.java
@@ -0,0 +1,12 @@
+package com.beginner;
+
+public class LoopFor {
+
+    public static void main(String[] args) {
+
+        for (int i = 0; i<=10; i++){
+            System.out.println("Hoba & "+i);
+        }
+        System.out.println("That's all");
+    }
+}
