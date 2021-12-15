6bef68b3d4a237689bad35ccf7e2e1849434e962
Ilya Barouski <birily9@gmail.com>
2021-12-15 22:38:35 +0300

    loopWhile

---
+++ javaBeginner/loopWhile/src/com/beginner/WhileLoops.java
@@ -0,0 +1,16 @@
+package com.beginner;
+
+public class WhileLoops {
+
+    public static void main(String[] args) {
+
+        int value = 0;
+        while (value<7){
+            System.out.println("Small step № "+value);
+            value = value+1;
+        }
+
+        System.out.print("...to big goal");
+
+    }
+}
