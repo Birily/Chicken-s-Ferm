3df3af160bc18eeeaf3868cb73c5e81cac1b3ff4
Ilya Barouski <birily9@gmail.com>
2021-12-22 23:04:31 +0300

    methodsInJava(method's parameters)

--- javaBeginner/methods/src/com/beginner/Methods.java
+++ javaBeginner/methods/src/com/beginner/Methods.java
@@ -10,17 +10,43 @@ public class Methods {
         person1.sayHello();
         System.out.println("Человеку до пенсии: "+person1.calculateYearsToRetirement()+" лет");
         System.out.println("Человеку до пенсии: "+person2.calculateYearsToRetirement()+" лет");
+        Person person3 = new Person();
+        person3.setNameAndAge("Игорь", 29);
+        person3.speak();
+        person3.setName("Артемон");
+        person3.speak();
+        person3.setAge(24);
+        person3.speak();
+        System.out.println("Nixya, pomolodel");
     }
 
 static class Person{
         String name;
         int age;
 
+        public Person(){
+
+        }
+
         public Person(String name, int age) {
             this.name = name;
             this.age = age;
         }
-        void speak(){
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setNameAndAge(String nameX,int ageX){
+            this.name = nameX;
+            this.age = ageX;
+    }
+
+    void speak(){
             System.out.println("Меня зовут "+this.name+", мне "+this.age+" лет");
         }
         void sayHello(){
