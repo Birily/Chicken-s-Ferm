d7a1e23685d40e472e099f57e18ccef92b1fc5f7
Ilya Barouski <birily9@gmail.com>
2021-12-22 22:41:52 +0300

    methodsInJava(returned Types)

--- javaBeginner/methods/src/com/beginner/Methods.java
+++ javaBeginner/methods/src/com/beginner/Methods.java
@@ -8,21 +8,27 @@ public class Methods {
         person1.speak();
         person2.speak();
         person1.sayHello();
+        System.out.println("Человеку до пенсии: "+person1.calculateYearsToRetirement()+" лет");
+        System.out.println("Человеку до пенсии: "+person2.calculateYearsToRetirement()+" лет");
     }
 
 static class Person{
-    String name;
-    int age;
+        String name;
+        int age;
 
-    public Person(String name, int age) {
-        this.name = name;
-        this.age = age;
-    }
-    void speak(){
-        System.out.println("Меня зовут "+this.name+", мне "+this.age+" лет");
-    }
-    void sayHello(){
-        System.out.println("Hello");
-    }
+        public Person(String name, int age) {
+            this.name = name;
+            this.age = age;
+        }
+        void speak(){
+            System.out.println("Меня зовут "+this.name+", мне "+this.age+" лет");
+        }
+        void sayHello(){
+            System.out.println("Hello");
+        }
+        int calculateYearsToRetirement(){
+            return 65-age;
+        }
+
     }
 }
