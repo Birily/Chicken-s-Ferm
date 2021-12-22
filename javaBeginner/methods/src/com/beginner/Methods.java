c1855daa227951ecbfbab11f1d8f1bbd8ea80f34
Ilya Barouski <birily9@gmail.com>
2021-12-22 23:23:33 +0300

    Encapsulation(getters and setters)

--- javaBeginner/methods/src/com/beginner/Methods.java
+++ javaBeginner/methods/src/com/beginner/Methods.java
@@ -4,44 +4,51 @@ public class Methods {
 
     public static void main(String[] args) {
         Person person1 = new Person("Василий", 43);
-        Person person2 = new Person("Виталик", 21);
+        Person person2 = new Person();
         person1.speak();
+        person2.setName("");
+        person2.setAge(-6);
         person2.speak();
-        person1.sayHello();
-        System.out.println("Человеку до пенсии: "+person1.calculateYearsToRetirement()+" лет");
-        System.out.println("Человеку до пенсии: "+person2.calculateYearsToRetirement()+" лет");
-        Person person3 = new Person();
-        person3.setNameAndAge("Игорь", 29);
-        person3.speak();
-        person3.setName("Артемон");
-        person3.speak();
-        person3.setAge(24);
-        person3.speak();
-        System.out.println("Nixya, pomolodel");
     }
 
 static class Person{
-        String name;
-        int age;
+        private String name;
+        private int age;
 
         public Person(){
 
         }
 
         public Person(String name, int age) {
-            this.name = name;
-            this.age = age;
+            this.setName(name);
+            this.setAge(age);
         }
 
     public void setName(String name) {
-        this.name = name;
+        if(name.isEmpty()){
+            System.out.println("Вы ввели пустое имя");
+        } else {
+            this.name = name;
+        }
+    }
+
+    public String getName() {
+        return name;
     }
 
     public void setAge(int age) {
-        this.age = age;
+        if(age<0){
+            System.out.println("Возраст не можент быть меньше нуля");
+        } else {
+            this.age = age;
+        }
     }
 
-    public void setNameAndAge(String nameX,int ageX){
+    public int getAge() {
+        return age;
+    }
+
+    public void setNameAndAge(String nameX, int ageX){
             this.name = nameX;
             this.age = ageX;
     }
