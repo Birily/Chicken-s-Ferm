a8c5c6e7981dc2b5f062313a3b258941714a76cc
Ilya Barouski <birily9@gmail.com>
2021-12-23 22:49:41 +0300

    key word static add counter

--- javaBeginner/worfThisAndConstructor/src/com/beginner/WordThisAndConstructor.java
+++ javaBeginner/worfThisAndConstructor/src/com/beginner/WordThisAndConstructor.java
@@ -3,33 +3,41 @@ package com.beginner;
 public class WordThisAndConstructor {
 
     public static void main(String[] args) {
-	Human human1 = new Human();
-	Human human2 = new Human("Leo");
-	Human human3 = new Human("Richard", 54);
-    human1.hi();
-    human2.hi();
-    human3.hi();
+        System.out.println("All humans in console is "+Human.getCounter());
+        Human human1 = new Human();
+        human1.hi();
+        System.out.println("All humans in console is "+Human.getCounter());
+        Human human2 = new Human("Leo");
+        human2.hi();
+        System.out.println("All humans in console is "+Human.getCounter());
+        Human human3 = new Human("Richard", 54);
+        human3.hi();
+        System.out.println("All humans in console is "+Human.getCounter());
     }
 }
 class Human{
     private String name;
     private int age;
+    private static int counter;
 
     public Human(){
         System.out.println("Hi, from default constructor");
         this.name = "Bob";
-        this.age = 1;
+        this.age = 18;
+        counter++;
     }
 
     public Human(String name){
         System.out.println("Hi, from 2nd constructor");
         this.setName(name);
+        counter++;
     }
 
     public Human(String name, int age) {
         System.out.println("Hi, from 3d constructor");
         this.setName(name);
         this.setAge(age);
+        counter++;
     }
 
     public String getName() {
@@ -49,5 +57,8 @@ class Human{
     }
     public void hi(){
         System.out.println("Hi, I'm "+this.name+" and my age is "+this.age);
+    }
+    public static int getCounter(){
+        return counter;
     }
 }
