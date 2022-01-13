c91ed7b6f5da71701cbb42b5d0c2a49333690b02
Ilya Barouski <birily9@gmail.com>
2022-01-13 21:29:08 +0300

    formatString

---
+++ javaBeginner/formatString/src/com/company/FormatString.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class FormatString {
+
+    public static void main(String[] args) {
+        System.out.printf("String %10d \n", 43);
+        System.out.printf("String %10.2f \n", 42.56784);
+        System.out.printf("String %10.4f \n", 14.45373);
+        System.out.printf("String %10s \n", "Steve");
+    }
+}
