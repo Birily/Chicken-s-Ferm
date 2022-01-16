0cb3b7a1279e203eecc4307451a9d5bcc4700cf6
Ilya Barouski <birily9@gmail.com>
2022-01-16 20:01:14 +0300

    packages

---
+++ javaBeginner/packages/src/Forest/Mushroom.java
@@ -0,0 +1,10 @@
+package Forest;
+
+public class Mushroom {
+    public Mushroom() {
+        this.grow();
+    }
+    public void grow(){
+        System.out.println("Mushroom is growing...");
+    }
+}
---
+++ javaBeginner/packages/src/Main.java
@@ -0,0 +1,9 @@
+import Forest.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Tree tree = new Tree();
+        Mushroom mushroom = new Mushroom();
+    }
+}
