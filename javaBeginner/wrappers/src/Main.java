bdd2830011b62a47d0c07e51ddd0ccf9d209c6f5
Ilya Barouski <birily9@gmail.com>
2022-01-17 22:27:51 +0300

    wrappers

---
+++ javaBeginner/wrappers/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(String[] args) {
+	int x = 1;
+    Integer x2 = new Integer(231);
+    int y =x2+Integer.parseInt("321");
+        System.out.println(y);
+
+    }
+}
