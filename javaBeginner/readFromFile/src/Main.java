3ed2da8d9f601ca5b808efa52822abdc34e329cc
Ilya Barouski <birily9@gmail.com>
2022-01-18 23:36:17 +0300

    Read From File Finish

--- javaBeginner/readFromFile/src/Main.java
+++ javaBeginner/readFromFile/src/Main.java
@@ -1,5 +1,6 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
@@ -15,6 +16,19 @@ public class Main {
             System.out.println(scanner.nextLine());
         }
         scanner.close();
+
+        File file1 = new File("src/java/org/Test2");
+        Scanner scanner1 = new Scanner(file1);
+        String line = scanner1.nextLine();
+        String[] numbersString = line.split(" ");
+        int[] numbers = new int[3];
+        int counter = 0;
+        for (String number : numbersString){
+            numbers[counter++] = Integer.parseInt(number);
+        }
+
+        System.out.println(Arrays.toString(numbers));
+        scanner1.close();
 
     }
 }
