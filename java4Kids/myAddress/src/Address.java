2942af6aed1e4f58275280d39c9131625b0f295a
Ilya Barouski <birily9@gmail.com>
2022-02-06 21:52:34 +0300

    address

---
+++ java4Kids/myAddress/src/Address.java
@@ -0,0 +1,8 @@
+public class Address {
+
+    public static void main(String[] args) {
+        System.out.println("17 Burdeynaha str., 12 apt. ");
+        System.out.println("Minsk ");
+        System.out.println("220018, Belarus");
+    }
+}
